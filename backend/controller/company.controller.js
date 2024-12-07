01ef3a25ce556e79ee2608c2f54e4cda7022dc0b
Github_explore <shreesha.g01@gmail.com>
2024-12-07 20:28:42 +0530

    add

--- backend/controller/company.controller.js
+++ backend/controller/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js"
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req,res) => {
     try{
@@ -77,7 +79,9 @@ export const updateCompany = async (req, res) => {
  
         const file = req.file;
         // cloudinary comes here
-
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
     
         const updateData = { name, description, website, location };
 
